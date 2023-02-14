7e9401ac4da860fbf97e1652acfcfde50a0fb729
Bruno Antunes <brunoantunes94@hotmail.com>
2023-02-14 02:24:46 -0300

    adicionando mui

--- Project/src/App.tsx
+++ Project/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Button } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,9 +25,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button variant="contained" color="primary">Hello World</Button>
     </div>
   )
 }
