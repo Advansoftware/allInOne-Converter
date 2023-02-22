533a8d4efb908c309461a6a0c87b43861d2b57b2
Bruno Antunes <brunoantunes94@hotmail.com>
2023-02-22 14:12:30 -0300

    removendo v1

--- Project/v1/ajax/removeFila.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('conexao.php');	
-	$id = $_GET['id'];	
-	$usuario = $_COOKIE['conversor'];
-	$del = mysql_query("DELETE FROM conversoes WHERE id = $id");
-	$flag = mysql_query("SELECT *  FROM conversoes WHERE convertido = 0 AND usuario = $usuario");
-	
-	echo mysql_num_rows($flag);
-?>
